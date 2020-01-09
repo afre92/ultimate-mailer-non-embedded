79a7e506fb143f1f7173ec2918c559a745596be0
Andres <afre92@gmail.com>
2020-01-09 13:55:23 -0500

    edit view qip

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,8 +1,21 @@
 class TemplateController < ApplicationController
+# before_action :find_shop
+
   def edit
     @template = Template.new
+    
   end
 
   def update
   end
+
+  private
+
+    def find_shop
+      # if params[:shop] && Shop.find(params[:shop]).present?
+      #   @shop = Shop.find(params[:shop])
+      # else
+         return render :nothing => true, :status => :bad_request
+      # end
+    end
 end
