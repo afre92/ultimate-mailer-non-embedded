b137f39284574dfd7040ec65e36db07a5a7cb822
Andres <afre92@gmail.com>
2020-02-04 19:41:41 -0500

    old model, controller and views deleted

--- app/models/thank_you_template.rb
+++
@@ -1,8 +0,0 @@
-class ThankYouTemplate < ApplicationRecord
-  before_save :replace_quote_entities_with_escape_characters
-
-  def replace_quote_entities_with_escape_characters
-    self.html = html.gsub("&quot;", "\"").gsub("&ldquo;", "\"").gsub("&rdquo;", "\"").gsub("&lsquo;", "\"").gsub("&rsquo;", "\"").gsub("&lt;", "<").gsub("&gt;", ">").gsub("&nbsp;", " ")
-    byebug
-	end
-end
