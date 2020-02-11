8e82e6e2d5caa5261f81c4252ea6320eeed7117d
Andres <afre92@gmail.com>
2020-02-10 20:54:37 -0500

    validating code within delimeters, replacing of characters moved to embedded email

--- app/models/template.rb
+++ app/models/template.rb
@@ -3,11 +3,10 @@
 class Template < ApplicationRecord
   belongs_to :shop
   validate :code
-  # before_save :replace_quote_entities_with_escape_characters
 
   def code
     new_html = html.gsub('&quot;', '"').gsub('&ldquo;', '"').gsub('&rdquo;', '"').gsub('&lsquo;', '"').gsub('&rsquo;', '"').gsub('&lt;', '<').gsub('&gt;', '>').gsub('&nbsp;', ' ')
-    elements = new_html.scan(/\<%(.*?)%>/).flatten
+    elements = new_html.scan(/\{{(.*?)}}/).flatten
     non_valid = []
     valid_code = self.valid_code
     elements.each do |ele|
@@ -15,10 +14,6 @@ class Template < ApplicationRecord
     end
     errors.add(:invalid, " #{non_valid.join(",")} ") if non_valid.present?
   end
-
-  # def replace_quote_entities_with_escape_characters
-  #   self.html = html.gsub('&quot;', '"').gsub('&ldquo;', '"').gsub('&rdquo;', '"').gsub('&lsquo;', '"').gsub('&rsquo;', '"').gsub('&lt;', '<').gsub('&gt;', '>').gsub('&nbsp;', ' ')#.gsub('{{', '<%=').gsub('}}', '%>')
-  # end
 
   def valid_code
     return %w( shop.email shop.address shop.owner_first_name shop.owner_last_name shop.phone shop.shop_name shop.title shop.website customer.email customer.first_name customer.last_name )
