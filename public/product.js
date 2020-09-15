2d4d7bd89d5724d30d47a9dd826acc328241f27f
Andres <afre92@gmail.com>
2020-09-14 21:52:26 -0400

    clean up in other files

--- public/product.js
+++ public/product.js
@@ -1,26 +1,59 @@
 
 // make sure this only loads on products page
-// Modal initializer
-$('body').on('click','.ue-close-modal',function() {
-  $('.ue-review-images-modal.visible').remove()
-});
-
-
-function getProductInfo(){
-  var product = $('#ProductJson-product-template').text();
-  return product = JSON.parse(product)
-}
-
 if ($('#ProductJson-product-template').length > 0) {
-  
-  var product = getProductInfo()
+  var product = getProductInfo();
   loadReviews(product);
   paginationHandler(product);
 };
 
+// close (remove) review images modal
+$('body').on('click','.ue-close-modal',function() {
+  $('.ue-review-images-modal').remove();
+});
+
+// show review photos modal
+$('body').on('click', '.review-photo',function(e){
+  var reviewId = $(this).parent('div.review-images-wrapper').data('review-id');
+  $.get( `${window.location.origin}/a/s`, {review_id: reviewId, method: 'review_images'})
+  .done(function(data){
+    $('body').append(data)
+    var slideIndex = 0;
+    showSlides(slideIndex);
+    return false;
+  })
+  .fail(function(){
+    console.log("something failed")
+  })
+})
+
+// handle review submittion from store front
+$('body').on('submit', 'form#ue-review-form',function(e){
+  e.preventDefault();
+  var product = getProductInfo()
+  $.get( `${window.location.origin}/a/s`, {product_title: product.title, review: $('form#ue-review-form').serializeArray(),product_id: product.id, method: "create_review_from_form"})
+  .done(function(data){
+
+    $('#ue-review-form-container').html(`
+    <div class="col-md-12" id="ue-review-submitted-container">
+      <div>
+        <span >THANK YOU FOR YOUR REVIEW !!!</span>
+      </div>
+    </div>`)
+    
+  })
+  .fail(function(){
+    console.log("something failed")
+  })
+})
+
+function getProductInfo(){
+  var product = $('#ProductJson-product-template').text();
+  return product = JSON.parse(product)
+};
+
 function loadReviews(product) {
   getReviews({ id: product.id, method: 'reviews' });
-}
+};
 
 function paginationHandler(product){
   $('body').on('click', '.ue-pagination > a',function(e){
@@ -28,8 +61,7 @@ function paginationHandler(product){
     var searchParams = new URLSearchParams($(this).attr('href'))
     getReviews({ id: product.id, page: searchParams.get('page'), method: 'reviews' })
   });
-}
-
+};
 
 function addRatingsBellowPrice(){
   // TODO: move this to a template
@@ -52,51 +84,7 @@ function addRatingsBellowPrice(){
                     </div>
                   </div><span>  ${median} Based on ${reviewsCount} reviews </span>`
   $('#ue-small-ratings-wrapper').html(template)
-}
-
-
-$('body').on('click', '.review-photo',function(e){
-  var reviewId = $(this).parent('div.review-images-wrapper').data('review-id');
-  
-  $.get( `${window.location.origin}/a/s`, {review_id: reviewId, method: 'review_images'})
-  .done(function(data){
-    //TODO: change path to not
-    // $('.ue-review-images-modal').addClass('visible');
-    $('body').append(data)
-  
-    var slideIndex = 0;
-    showSlides(slideIndex);
-
-    return false;
-    
-    // try to find source code for modal and if not then add bootrap js :/
-    // paste modal and show
-  })
-  .fail(function(){
-    console.log("something failed")
-  })
-})
-
-
-$('body').on('submit', 'form#review-form',function(e){
-  e.preventDefault();
-  var product = getProductInfo()
-  $.get( `${window.location.origin}/a/s`, {product_title: product.title, review: $('form#review-form').serializeArray(),product_id: product.id, method: "create_review_from_form"})
-  .done(function(data){
-
-
-    $('#ue-review-form-container').html(`
-    <div class="col-md-12" id="ue-review-submitted-container">
-      <div>
-        <span >THANK YOU FOR YOUR REVIEW !!!</span>
-      </div>
-    </div>`)
-    
-  })
-  .fail(function(){
-    console.log("something failed")
-  })
-})
+};
 
 function getReviews(params){
   $.get( `${window.location.origin}/a/s`, params)
@@ -115,7 +103,6 @@ function getReviews(params){
 }
 
 // Js for slide show
-
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -139,7 +126,7 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-}
+};
 
 
 $('body').on('click', '#ue-open-review-form-modal',function(e){
